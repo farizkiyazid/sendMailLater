ba50b7c6edc6a51f61c0e60eb3fcc01c48fa7375
farizkiyazid <25681385+farizkiyazid@users.noreply.github.com>
2020-01-02 12:43:38 +0700

    add readme

--- app/README.md
+++ app/README.md
@@ -1,2 +1,14 @@
 # sendMailLater
 Flask app that sends POST request of an email and sends it on the specified time we want.
+
+<!-- to run the app -->
+flask run
+
+<!-- to make database -->
+flask db init
+
+<!-- to create database migration -->
+flask db migrate
+
+<!-- to update the migration to the database -->
+flask db upgrade
--- config.py
+++ config.py
@@ -3,6 +3,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    # this database i use is from heroku postgres
     SQLALCHEMY_DATABASE_URI = 'postgres://hccwclzwgseygw:6b0825488e4175031a47a7917be4b51393a908cc5afa72fae7c7886b5faa6445@ec2-174-129-33-186.compute-1.amazonaws.com:5432/d2bhvua8dpvqda'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
